2ab79850a3475c92a5b254a6b44081b3dbccdbf8
Aaron Emerson <aaronemerson@Aarons-MacBook-Pro.local>
2023-08-26 18:40:06 +1000

    updated stringtool

--- StringTool.java
+++ StringTool.java
@@ -4,6 +4,24 @@ public class StringTool {
         return str.toUpperCase();
     }
 
+    public static String toTitleCase(String input) {
+        StringBuilder titleCase = new StringBuilder(input.length());
+        boolean nextTitleCase = true;
+
+        for (char c : input.toCharArray()) {
+            if (Character.isSpaceChar(c)) {
+                nextTitleCase = true;
+            } else if (nextTitleCase) {
+                c = Character.toTitleCase(c);
+                nextTitleCase = false;
+            }
+
+            titleCase.append(c);
+        }
+
+        return titleCase.toString();
+    }
+
     public static void main(String[] args) {
 
     }
